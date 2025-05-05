f87031e672f1d21e9592ff2456edd5ba87bb3e52
emafina <emanuele.finazzi@gmail.com>
2025-05-05 08:54:38 +0200

    feat: added mutating data

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,9 +8,14 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 
+import { createInvoice } from '@/app/lib/actions';
+
+// action in form: in react, behind the scenes this creates a POST api endpoint, which
+// has not to be created manually
+
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice} >
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,4 +1,5 @@
 'use client';
+// not clear why it is a client component. It seems to work also deleting the directive..
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
@@ -10,6 +11,8 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+import { updateInvoice } from '@/app/lib/actions';
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +20,15 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+
+  // Note: Using a hidden input field in your form also works
+  // (e.g. <input type="hidden" name="id" value={invoice.id} />).
+  // However, the values will appear as full text in the HTML source,
+  // which is not ideal for sensitive data.
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
